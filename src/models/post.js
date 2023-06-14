1a96da6219b90abfda21a3865ef523578af31dc9
Victor Demetrius Marqueto <73915190+VictorDemetrius@users.noreply.github.com>
2023-06-13 23:08:44 -0300

    Add files via upload

---
+++ src/models/post.js
@@ -0,0 +1,37 @@
+const db = require('./banco')
+
+//Criando o molde de uma tabela do banco
+const Agendamentos = db.sequelize.define("cadastros", {
+
+    nome:{
+
+        type: db.Sequelize.STRING
+    },
+    telefone:{
+        type: db.Sequelize.STRING
+    },
+    origem:{
+        type: db.Sequelize.STRING
+    },
+    data:{
+        type: db.Sequelize.STRING
+    },
+    observacao:{
+        type: db.Sequelize.TEXT
+    }
+    
+})
+
+// Agendamentos.sync({force: true})
+
+Agendamentos.create({
+    nome: "Roberval Silva",
+     endereco: "Avenida Raposo Tavares",
+     bairro: "Cocuera",
+     cep: 00000000,
+     cidade: "Mogi das Cruzes",
+     estado: "São Paulo",
+     observacao: "Não há observações O.O"
+ })
+
+module.exports = Agendamentos
